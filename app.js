988ad4e21cdfcb603b0492b0fed17b5cea9289bd
Alex <asmyshlyaev177@gmail.com>
2017-12-02 22:23:41 +0300

    start figuring out how mongoose work

--- app.js
+++ app.js
@@ -4,11 +4,23 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/todo',
+  {useMongoClient: true, reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 500, keepAlive: true});
+var db = mongoose.connection;
+var Schema = mongoose.Schema;
+
+db.on('error', console.error.bind(console, 'connection error'));
+db.on('open', function() { console.log('connected to db!')});
 
 var index = require('./routes/index');
 var todo = require('./routes/todo');
 
 var app = express();
+
+app.set('db', mongoose);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -17,9 +29,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'jade');
 
-app.use('/', index);
+// app.use('/', index);
 app.use('/todo', todo);
+
+var schema = new mongoose.Schema({ name: 'string', size: 'string' });
+var Tank = mongoose.model('Tank', schema);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,7 +53,14 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.end(err.message);
+});
+
+process.on('SIGINT', function() {  
+  mongoose.connection.close(function () { 
+    console.log('Mongoose default connection disconnected through app termination'); 
+    process.exit(0); 
+  }); 
 });
 
 module.exports = app;
--- routes/todo.js
+++ routes/todo.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/:id', function(req, res, next) {
-  res.end(JSON.stringify(req.params));
+router.get('/:id', async function(req, res, next) {
+  var r = await req.app.get('db').model('Tank').find();
+  res.end(JSON.stringify(r));
+  // req.app.get('db').model('Tank').find({}, function(err, data) {
+  //   if (err) handleErr(err);
+  //   res.end(JSON.stringify(data));
+  // });
 });
 
 module.exports = router;

908cbc788885bd338b33217e5c2a2a927d92514d
Alex <asmyshlyaev177@gmail.com>
2017-12-03 14:23:33 +0300

    .

--- routes/todo.js
+++ routes/todo.js
@@ -13,25 +13,25 @@ router.get('/', function(req, res, next) {
 
 router.get('/:id', function(req, res, next) {
   Todo.findById(req.params.id, function(err, data) {
-    if (err) { return res.end(JSON.stringify(err)) };
-    res.end(JSON.stringify(data.toObject()) );
+    if (err) { return res.json(err); };
+    res.end(JSON.stringify(data ? data.toObject() : {message: 'not found'}) );
   });
 });
 
 router.post('/', function(req, res, next) {
+  if (!req.body.title) { return res.json({message: 'title is required!'}) };
   Todo.create({ title: req.body.title }, function(err, data) {
     if (err) { return res.json(err) };
     res.end(JSON.stringify(data));
   });
 });
 
-router.patch('/:id', async function(req, res, next) {
-  let result;
+router.patch('/:id', function(req, res, next) {
   Todo.findById(req.params.id, function(err, doc) {
     if (err) { return res.json(err) };
-    doc.title = req.body.title;
-    console.log(doc.title);
+    doc.title = req.body.title || doc.title;
     doc.edited = (new Date).toISOString();
+    doc.completed = req.body.completed || doc.completed;
     doc.save()
       .then(doc => res.json(doc.toObject()))
       .catch(err => res.json(err));
@@ -39,7 +39,7 @@ router.patch('/:id', async function(req, res, next) {
 });
 
 router.delete('/:id', function(req, res, next) {
-  req.app.get('db').model('Tank').find({}, function(err, data) {
+  Todo.deleteOne({ id: req.params.id }, function(err, data) {
     if (err) { return res.json(err) };
     res.end(JSON.stringify(data));
   });

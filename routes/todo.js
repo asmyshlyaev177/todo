9bbc73692cf03ac51a7818631ba94ebd1d175bb7
Alex <asmyshlyaev177@gmail.com>
2017-12-03 15:27:21 +0300

    .

--- routes/todo.js
+++ routes/todo.js
@@ -6,42 +6,42 @@ router.get('/', function(req, res, next) {
   // var r = await req.app.get('db').model('Tank').find();
   // res.end(JSON.stringify( r.map(r => r.toObject()) ));
   Todo.find({}, function(err, data) {
-    if (err) { return res.status(500).json(err) };
-    res.end(JSON.stringify(data.map(d => d ? d.toObject(): null) ));
+    if (err) { return res.status(500).json({ error: err.message }) };
+    res.json(data.map(d => d ? d.toObject(): null) );
   });
 });
 
 router.get('/:id', function(req, res, next) {
   Todo.findById(req.params.id, function(err, data) {
-    if (err) { return res.status(500).json(err); };
-    res.end(JSON.stringify(data ? data.toObject() : {message: 'not found'}) );
+    if (err) { return res.status(500).json({ error: err.message }); };
+    data ? res.json(data.toObject()) : res.status(500).json({ message: 'not found' });
   });
 });
 
 router.post('/', function(req, res, next) {
-  if (!req.body.title) { return res.status(500).json({message: 'title is required!'}) };
+  if (!req.body.title) { return res.status(500).json({error: 'title is required!'}) };
   Todo.create({ title: req.body.title }, function(err, data) {
-    if (err) { return res.status(500).json(err) };
-    res.end(JSON.stringify(data));
+    if (err) { return res.status(500).json({ error: err.message }) };
+    res.json(data);
   });
 });
 
 router.patch('/:id', function(req, res, next) {
   Todo.findById(req.params.id, function(err, doc) {
-    if (err) { return res.status(500).json(err) };
+    if (err) { return res.status(500).json({ error: err.message }) };
     doc.title = req.body.title || doc.title;
     doc.edited = (new Date).toISOString();
     doc.completed = req.body.completed || doc.completed;
     doc.save()
       .then(doc => res.json(doc.toObject()))
-      .catch(err => res.status(500).json(err));
+      .catch(err => res.status(500).json({ error: err.message }));
   })
 });
 
 router.delete('/:id', function(req, res, next) {
   Todo.deleteOne({ _id: req.params.id }, function(err, data) {
-    if (err) { return res.status(500).json(err) };
-    res.end(JSON.stringify(data));
+    if (err) { return res.status(500).json({ error: err.message }) };
+    res.json(data);
   });
 });
 

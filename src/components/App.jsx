1ad8e68abfef123b6b38543199c354856cb6de4f
Alex <asmyshlyaev177@gmail.com>
2017-12-19 14:12:20 +0300

    Btns component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from 'components/Header'
 import Todo from 'components/Todo'
+import Btns from 'components/Btns'
 
 class App extends React.Component {
   constructor(props) {
@@ -13,12 +14,7 @@ class App extends React.Component {
 
         <Header />
 
-        <div className="container has-text-centered add-todo-container">
-          <div>Todo count: 5</div>
-          <a className="button is-success is-outlined is-rounded">Add todo</a>
-          <a className="button is-danger is-outlined is-rounded">Clean completed</a>
-        </div>
-
+        <Btns />
 
         <div className="container is-centered is-fluid">
 

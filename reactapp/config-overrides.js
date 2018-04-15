8c3c4c90eb03e9775280d87a0a8379e505d8ff41
Alex <asmyshlyaev177@gmail.com>
2018-04-15 20:12:57 +0300

    scss support

--- reactapp/config-overrides.js
+++ reactapp/config-overrides.js
@@ -4,5 +4,9 @@ const {injectBabelPlugin} = require('react-app-rewired');
 module.exports = function override(config, env) {
   // add a plugin
   config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], config);
+  config.module.rules.unshift({
+    test: /\.scss$/,
+    loaders: ["style", "css", "sass"]
+  })
   return config;
 }
